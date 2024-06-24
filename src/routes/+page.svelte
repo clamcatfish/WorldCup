fb86b60e43d1eab220fe32f17585b2b4dedeeefc
calum <clam0540@gmail.com>
2024-06-24 12:39:16 -0600

    setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-green-600">Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<a href="/about">About</a>
+<p>Hi Mavis!</p>
+
+<style>
+  a {
+    font-family: sans-serif;
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<h1>About</h1>
+<a href="/">Home</a>
+
+<style>
+  h1 {
+    font-family: "Noto Serif", serif;
+  }
+
+  a {
+    font-family: sans-serif;
+  }
+</style>
